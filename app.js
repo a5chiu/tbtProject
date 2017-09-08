088b55582273ff14598209e4c25eee8a895d6dd6
a5chiu <a.chiu1997@yahoo.com>
2017-09-08 10:31:18 -0700

    finished

--- app.js
+++ app.js
@@ -3,26 +3,100 @@
 var express = require('express');
 var path = require('path');
 var reload = require('reload');
+var mongo = require('mongodb');
+var assert = require('assert');
+var bodyParser = require('body-parser');
+var nodemailer = require('nodemailer');
+
+//url to store database
+var url = "mongodb://tbtowner:Cinnamon1521724@ds143030.mlab.com:43030/tbtserver";
 
 //calling it into app
 var app = express();
 
-//sends html file
-app.get('/', function(req, res) {
-    res.status(200).sendFile(__dirname + '/index.html');
-});
+var router = express.Router();
 
-app.post('/', function(req, res) {
-    console.log("PRINT");
-});
+app.use(bodyParser.urlencoded({ extended: false }));
 
-//loads static files (css and javascript: exclusing main js)
-app.use('/public', express.static(path.join(__dirname, 'public')));
 
 //connects to the server of port 8080
 var server = app.listen(process.env.PORT || 8080, function(req, res) {
     console.log("App listening on Port %s", server.address().port);
     console.log("test");
 });
+
+
+//transporter for email
+var transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    secure: true,
+    port: 465,
+    auth: {
+        user: 'alexchiu1901@gmail.com',
+        pass: 'Cinnamon1521724'
+    },
+});
+
+
+//loads static files (css and javascript: exclusing main js)
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+//sends html file
+app.get('/', function(req, res) {
+    res.status(200).sendFile(__dirname + '/index.html');
+});
+
+app.post('/Submitted', function(req, res) {
+
+    var item = {
+        //tite of name in form (req.body.NAME)
+        first_name: req.body.firstName,
+        last_name: req.body.lastName,
+        phone_number: req.body.phoneNumber,
+        email: req.body.email,
+        subject: req.body.subject,
+        description: req.body.description
+    };
+
+    console.log("stage 2");
+    //connecting to database
+    //function has error if error, or database if successful
+    mongo.connect(url, function(error, database) {
+        assert.equal(null, error);
+
+        //calls collection database and inserts one item
+        //insertOne has call back fuction to see if it was successful or error
+        database.collection('customerInfo').insertOne(item, function(err, result) {
+            //if err is null, it is okay!
+            assert.equal(null, err);
+
+            console.log("insertion successful");
+
+            //close database
+            database.close();
+
+            res.sendFile(__dirname + '/accepted.html');
+        });
+    });
+
+    var message = "First Name: " + item.first_name + "\n" + "Last Name: " + item.last_name +
+        "\n" + "Phone Number: " + item.phone_number + "\n" + "Email: " + item.email + "\n" +
+        "Subject: " + item.subject + "\n" + "Description: " + item.description;
+
+    var mailOptions = {
+        from: '"Alex Chiu" <alexchiu1901@gmail.com>',
+        to: 'a5chiu@ucsd.edu',
+        subject: 'TBT EMAIL BOT',
+        text: message
+    };
+
+    transporter.sendMail(mailOptions, function(error, info) {
+        assert.equal(null, error);
+
+        console.log("Email Sent Successfully");
+    });
+
+});
+
 
 reload(app);
--- public/client.js
+++ public/client.js
@@ -1,7 +1,12 @@
 var x = (function() {
 
     var slideIndex = 0;
-    var totalSlides = 2;
+    var totalSlides = 5;
+    var numRequiredForms = 5;
+
+    var locationOpen = false;
+    var contactOpen = false;
+    var serviceOpen = false;
 
 
     function plusSlides(n) {
@@ -24,17 +29,172 @@ var x = (function() {
         $(".mySlides").eq(n).show();
         $(".dot").removeClass("active");
         $(".dot").eq(n).addClass("active");
+    }
+
+    function validateForm() {
+        var errorString = "";
+        var pass = true;
+
+        //checks if any inputs are empty
+        $("input").each(function(index, val) {
+            if (val.value == "") {
+                errorString = errorString + val.placeholder + ": Cannot be Empty" + "\n";
+                pass = false;
+            }
+        });
+
+        //validate phone helper function
+        var phone = validatePhone();
+
+        if (!phone) {
+            pass = false;
+            errorString = errorString + "Invalid Phone Number, XXX-XXX-XXXX format please" + "\n";
+        }
+
+        var email = validateEmail();
+
+        if (!email) {
+            pass = false;
+            errorString = errorString + "Invalid Email" + "\n";
+        }
+
+        if (errorString) {
+            alert(errorString);
+            return false;
+        };
 
     }
+
+    //helper function to validate phone numbers
+    function validatePhone() {
+        var phoneno = /^[2-9]\d{2}-\d{3}-\d{4}$/;
+        var inputNum = $("#phoneNumber").val();
+
+        return phoneno.test(inputNum);
+    }
+
+    //for Safari
+    function validateEmail() {
+        var emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        var inputEmail = $("#email").val();
+
+        return emailRegex.test(inputEmail);
+    }
+
+    function toggleButton(id, arrow) {
+        $(id).hover(function() {
+            $(this).css({
+                "cursor": "pointer",
+            });
+
+            $(this).stop(true, false).animate({
+                height: "3.5vw",
+            });
+            $(arrow).stop(true, false).slideToggle(500);
+        }, function() {
+            $(this).stop(true, false).animate({
+                height: "2.2vw",
+            });
+            $(arrow).stop(true, false).slideToggle(500);
+        });
+    }
+
+
+    function toggleInfo(id, expand) {
+        $(id).click(function() {
+            $(expand).slideToggle(650);
+        });
+    }
+
+    function hoverMouse(id) {
+        $(id).hover(function() {
+            $(this).css("cursor", "pointer");
+        });
+    }
+
+    function scrollTo(clicked, position, open) {
+        $(clicked).click(function() {
+            $('html,body').animate({
+                    scrollTop: $(position).offset().top
+                },
+                'slow');
+            if (open != null) {
+                $(open).delay(700).slideDown(650);
+            }
+        });
+    }
+
 
     return {
         "plusSlides": plusSlides,
         "currentSlide": currentSlide,
         "showSlides": showSlides,
+        "validateForm": validateForm,
+        "toggleButton": toggleButton,
+        "toggleInfo": toggleInfo,
+        "initMap": initMap,
+        "hoverMouse": hoverMouse,
+        "scrollTo": scrollTo
+
     };
 
 })();
 
+var map;
+var uluru;
+
+function initMap() {
+
+    uluru = {
+        lat: 34.081028,
+        lng: -118.059239
+    };
+    map = new google.maps.Map($("#map")[0], { zoom: 15, center: uluru });
+
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+    });
+
+}
+
 $(document).ready(function() {
+
+    //next button , prev button
     $("#next").click(function() { return x.plusSlides(1); });
+    $("#prev").click(function() { return x.plusSlides(-1); });
+
+    //current slide for dot
+    $("#dot1").click(function() { return x.currentSlide(0); });
+    $("#dot2").click(function() { return x.currentSlide(1); });
+    $("#dot3").click(function() { return x.currentSlide(2) });
+    $("#dot4").click(function() { return x.currentSlide(3) });
+    $("#dot5").click(function() { return x.currentSlide(4) });
+
+    //submit button hover, nav button hover
+    x.hoverMouse("#submitButton");
+    x.hoverMouse("#contactNav");
+    x.hoverMouse("#serviceNav");
+    x.hoverMouse("#scheduleNav");
+    x.hoverMouse("#locationNav");
+    x.hoverMouse("#aboutNav");
+
+    //toggling buttons
+    x.toggleButton("#contactButton", "#downArrowContact");
+    x.toggleButton("#locationButton", "#downArrowLocation");
+    x.toggleButton("#aboutButton", "#downArrowAbout");
+
+    //toggling info
+    x.toggleInfo("#contactButton", "#contactHidden");
+    x.toggleInfo("#aboutButton", "#aboutHidden");
+
+    //scrolling to info for nav
+    x.scrollTo("#contactNav", "#contact");
+    x.scrollTo("#serviceNav", "#Service");
+    x.scrollTo("#locationNav", ".Location");
+    x.scrollTo("#aboutNav", ".About", "#aboutHidden");
 });
+
+window.onload = function() {
+    x.currentSlide(0);
+}
